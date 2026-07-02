0e31beb82fecb640d32bf413632c4a616f8f8915
passerby <1184742350@qq.com>
2026-07-02 17:20:42 +0800

    Delete index.py

--- index.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-# 将 theta_project 目录加入 Python 模块搜索路径
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'theta_project'))
-
-# 从 theta_project/asgi.py 中导入你的 FastAPI 实例 app
-from asgi import app
